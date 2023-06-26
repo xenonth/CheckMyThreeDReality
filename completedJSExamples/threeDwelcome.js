6ca707dc3a664b9c416cc26bdc816315619be0e9
xenonth <timothy.forbes259@gmail.com>
2023-06-26 15:55:22 +0930

    Simplified loader.load to figure out why font doesn't want to load

--- completedJSExamples/threeDwelcome.js
+++ completedJSExamples/threeDwelcome.js
@@ -1,19 +1,45 @@
 import * as THREE from 'three';
 
-import WebGL from "three/addons/capabilities/WebGL.js"
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 
-//imports for threed text.
-import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
-import helvetikerFont from 'three/examples/fonts/helvetiker_regular.typeface.json';
+import typefaceFont from 'three/examples/fonts/helvetiker_regular.typeface.json'
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-//redndering the font
-const font = new THREE.Font(helvetikerFont);
+const loader = new FontLoader();
 
-const geometry = new THREE.TextBufferGeometry()
+const font = loader.load(
+	// resource URL
+	typefaceFont,
+
+	// onLoad callback
+	function ( font ) {
+		// do something with the font
+		console.log( "Armed and loaded" );
+	},
+
+	// onProgress callback
+	function ( xhr ) {
+		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+	},
+
+	// onError callback
+	function (error) {
+		console.log( `text Resource failed to load` );
+	}
+);
+
+camera.position.z = 5;
+
+//animate the text to render to the screen
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
