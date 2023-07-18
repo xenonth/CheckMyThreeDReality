5fefdcd89f304037e772995af537f9467fd0c62c
xenonth <timothy.forbes259@gmail.com>
2023-07-18 12:25:20 +0930

    applying Euler to the central y axis

--- completedJSExamples/understandingQuaternions/normalizeVectors.js
+++ completedJSExamples/understandingQuaternions/normalizeVectors.js
@@ -14,13 +14,19 @@ const v_dir = new THREE.Vector3(0,0,18).normalize();
 console.log(v_dir.x,v_dir.y,v_dir.z);
 
 //object
+//adding a grid for visual reference
+scene.add(new THREE.GridHelper(6,6));
+//using multiply scalar to set unit length, and apply euler to change direction
+[[1.00,0],[1.25, 45],[1.50, 90],[1.75, 135],[2.00, 180],[2.25, 225],[2.75, 270],[3.00, 375]].forEach((data) => {
+    const length = data[0], degree = data[1];
+    const mesh = new THREE.Mesh( new THREE.SphereGeometry(0.0075, 10, 10));
+    const e = new THREE.Euler();
+    e.y = THREE.MathUtils.degToRad(degree)
+    mesh.position.copy(v_dir).applyEuler(e).multiplyScalar(length);
+    scene.add(mesh);
+})
 
-scene.add(new THREE.GridHelper(2,2));
-const mesh1 = new THREE.Mesh(new THREE.SphereGeometry(0.1, 10,10));
-scene.add(mesh1);
-mesh1.position.copy(v_dir);
-
-camera.position.set(2,2,2);
+camera.position.set(3,4,3);
 camera.lookAt(0,0,0);
 
 
