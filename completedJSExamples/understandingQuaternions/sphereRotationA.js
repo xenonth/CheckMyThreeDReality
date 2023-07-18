d9f13866f5a75edceee669b82d227922f8369a76
xenonth <timothy.forbes259@gmail.com>
2023-07-18 15:27:03 +0930

    understanding setRotation by axis

--- completedJSExamples/understandingQuaternions/sphereRotationA.js
+++ completedJSExamples/understandingQuaternions/sphereRotationA.js
@@ -9,14 +9,33 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 scene.add(new THREE.GridHelper(10, 10));
+//Helper functions
+
+//rotation function designed to teach quaternions in three.js
+const setRotationByAxis = (q, v_axis, n_degree) => {
+    const vector = v_axis.normalize();
+    const radian = THREE.MathUtils.degToRad(n_degree);
+    const halfAngle = radian/ 2, s = Math.sin(halfAngle);
+    q.x = vector.x * s;
+    q.y = vector.y * s;
+    q.z = vector.z * s;
+    q.w = Math.cos(halfAngle);
+}
+
 // Sphere
-const mesh1 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial());
+const mesh1 = new THREE.Mesh(new THREE.CylinderGeometry(0, 0.25, 1), new THREE.MeshNormalMaterial());
+mesh1.geometry.rotateX(Math.PI * 0.5);
 scene.add(mesh1);
+
 // setting rotation with quaternion
-const axis = new THREE.Vector3(1,0,0);
+const q = new THREE.Quaternion();
+const v_axis = new THREE.Vector3(0, 10, 0);
 const degree = 45;
 
-mesh1.quaternion.setFromAxisAngle(axis.normalize(), THREE.MathUtils.degToRad(degree));
+setRotationByAxis(q, v_axis, degree);
+mesh1.rotation.setFromQuaternion(q);
+
+//mesh1.quaternion.setFromAxisAngle(axis.normalize(), THREE.MathUtils.degToRad(degree));
 
 // Camera position
 camera.position.set(2, 2, 2);
